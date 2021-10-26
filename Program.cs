9b9404972febd56560e29c6b8ecec7d3e57e8a82
AHAD RAZA <83767844+aahadraza@users.noreply.github.com>
2021-10-26 09:12:01 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace create;
+ 
+  public int AddNumbers(int number1,
+                        int number2)
+{
+    int result = number1 + number2;
+    if (result > 10)
+    {
+        return result;
+    }
+    return 0;
+}
